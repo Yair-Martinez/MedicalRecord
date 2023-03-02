fd9cbf5b4e27cebfb8811942a4a272ce2fddb9d3
Yair Martinez <yairmartinezespitia@gmail.com>
2023-03-02 11:46:23 -0500

    Configuracion inicial de carpetas y dependencias

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  host: process.env.HOST_DB,
+  user: process.env.USER_DB,
+  password: process.env.PASSWORD_DB,
+  database: process.env.DATABASE_DB,
+  port: process.env.PORT_DB,
+});
+
+module.exports = pool;
---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const routes = require('./routes/index.routes.js');
+
+const PORT = process.env.PORT;
+
+// MIDDLEWARES
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(routes);
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}... 🚀🚀`);
+});
